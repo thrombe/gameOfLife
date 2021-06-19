9ecdd5197050799db600a899c82417f472d93bbd
thrombe <yash.n.thombare620@gmail.com>
2021-06-19 11:04:38 +0000

    smol changes

--- rustGOL/src/gol.rs
+++ rustGOL/src/gol.rs
@@ -24,7 +24,7 @@ pub fn gol() {
     ncurses::initscr();
     print_reset(&mut cells, &mut indices, &mut board, LEN);
     for _ in 0..200 {
-        cells_set(&mut cells, &indices, WIDTH);
+        cells_set(&mut cells, &indices, WIDTH, LEN);
         print_reset(&mut cells, &mut indices, &mut board, LEN);
     }
     ncurses::endwin();
@@ -32,14 +32,13 @@ pub fn gol() {
 
 // cell++ for every neighbor or alive cells
 #[inline(always)]
-fn cells_set(cells: &mut [u8], indices: &[i32], width: usize) {
+fn cells_set(cells: &mut [u8], indices: &[i32], width: usize, len: usize) {
     let around: [(i32, i32); 8] = [(-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1), (-1, 0)];
     for &i in indices.iter() {
-        if i == -1 {break} // signals end of live cells
-        // go around i and do ++ (torus)
-        for (j, k) in around.iter() {
+        if i == -1 {break} // end of live cells
+        for (j, k) in around.iter() { // go around i and do ++ (torus)
             let index = ((i as i32)+j + (width as i32)*k) as usize;
-            if index >= cells.len() {continue} // broken slightly
+            if index >= len {continue} // broken slightly
             cells[index] += 0b0000_0001;
         }
     }
@@ -51,8 +50,7 @@ fn print_reset(cells: &mut [u8], indices: &mut [i32], board: &mut [u8], len: usi
     let mut i_indices: usize = 0;
     let mut i_board: usize = 0;
     for i_cells in 0..len {
-        // set cell
-        match cells[i_cells] & 0b0000_1111 {
+        match cells[i_cells] & 0b0000_1111 { // set cell
             3 => cells[i_cells] = 0b0001_0000,
             2 => cells[i_cells] = cells[i_cells] & 0b0001_0000,
             _ => cells[i_cells] = 0b0000_0000,
@@ -63,14 +61,14 @@ fn print_reset(cells: &mut [u8], indices: &mut [i32], board: &mut [u8], len: usi
             i_indices += 1;
             board_set(&mut i_board, board, b'x');
         } else { // if dead
-            board_set(&mut i_board, board, b'.');
+            board_set(&mut i_board, board, b' ');
         }
         i_board += 1;
     }
     indices[i_indices] = -1; // will crash if every cell is alive but whatever
     
     // println!("{}", std::str::from_utf8(&board).unwrap());
-    ncurses::erase();
+    ncurses::erase(); // seems like this is the slowest part of the code
     ncurses::addstr(std::str::from_utf8(&board).unwrap());
     ncurses::refresh();
 }
