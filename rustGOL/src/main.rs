41b0423ef72d433a6b02479b7a4fb0d0567acc7c
thrombe <yash.n.thombare620@gmail.com>
2021-06-21 14:10:50 +0000

    more smol changes

--- rustGOL/src/main.rs
+++ rustGOL/src/main.rs
@@ -1,19 +1,17 @@
 #![allow(dead_code)]
 #![allow(non_snake_case)]
-// #![allow(unused_assignments)]
 
 mod gol;
 mod bbrain;
 mod import_structure;
 use std::time;
+use std::env; // for command line arguments
 
 pub const WIDTH: usize = 151;
 pub const HEIGHT: usize = 160;
 pub const TORUS: bool = true;
 pub const NCURSED: bool = true;
 pub const LOOPS: usize = 200;
-
-use std::env;
 
 fn main() {
     let now = time::Instant::now();
